c89341f38ee2a48a1c298090773d516ecb16165b
Toryang <toryangchen@outlook.com>
2019-11-27 19:34:58 +0800

    nav导航

--- src/components/default/NavBar.vue
+++ src/components/default/NavBar.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="navbar">navbar</div>
+  <div class="navbar">
+    <breadcrumb class="breadcrumb-container"></breadcrumb>
+  </div>
 </template>
 
 <script>
+import Breadcrumb from "./Breadcrumb";
 export default {
-  name: "navbar"
+  name: "navbar",
+  components: {
+    Breadcrumb,
+  },
 };
 </script>
 <style lang="less" scoped>
 .navbar {
+  overflow: hidden;
+  position: relative;
   height: 50px;
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+
+  .breadcrumb-container {
+    float: left;
+  }
 }
 </style>
